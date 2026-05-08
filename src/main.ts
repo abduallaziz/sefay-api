d78d5d6dae872538122860c3067070760723cc1d
abduallaziz <abduallaziz.altyar@gmail.com>
2026-05-09 00:49:17 +0300

    fix: cors origin true

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://sefay-web.vercel.app',
-      '*',
-    ],
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
