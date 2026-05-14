1752f0786764292de0827e1fe7e93a3ae3cdfa7c
abduallaziz <abduallaziz.altyar@gmail.com>
2026-05-14 10:01:39 +0300

    feat: appointments module complete

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
+  imports: [SupabaseModule],
   providers: [AppointmentsService],
   controllers: [AppointmentsController],
 })
-export class AppointmentsModule {} 
+export class AppointmentsModule {}
